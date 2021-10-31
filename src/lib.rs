522faff33b0d8aa0940fdc9764aef1353a2187ef
Emil Fresk <emil.fresk@gmail.com>
2021-10-31 09:07:04 +0100

    New trait and time lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,20 @@
 //! # `Monotonic` implementation based on SysTick
+//!
+//! Uses [`fugit`] as underlying time library.
+//!
+//! [`fugit`]: https://docs.rs/crate/fugit
 
 #![no_std]
 
 use cortex_m::peripheral::{syst::SystClkSource, SYST};
-use rtic_monotonic::{
-    embedded_time::{clock::Error, fraction::Fraction},
-    Clock, Instant, Monotonic,
-};
+pub use fugit::{self, ExtU64};
+use rtic_monotonic::Monotonic;
 
-/// Systick implementing `embedded_time::Clock` and `rtic_monotonic::Monotonic` which runs at a
+/// Systick implementing `rtic_monotonic::Monotonic` which runs at a
 /// settable rate using the `TIMER_HZ` parameter.
 pub struct Systick<const TIMER_HZ: u32> {
     systick: SYST,
-    cnt: u32,
-    reload: u32,
+    cnt: u64,
 }
 
 impl<const TIMER_HZ: u32> Systick<TIMER_HZ> {
@@ -22,48 +23,53 @@ impl<const TIMER_HZ: u32> Systick<TIMER_HZ> {
     /// Note that the `sysclk` parameter should come from e.g. the HAL's clock generation function
     /// so the real speed and the declared speed can be compared.
     pub fn new(mut systick: SYST, sysclk: u32) -> Self {
+        let reload = (sysclk + TIMER_HZ / 2) / TIMER_HZ - 1;
+
         systick.disable_counter();
+        systick.set_clock_source(SystClkSource::Core);
+        systick.set_reload(reload);
 
-        Systick {
-            systick,
-            cnt: 0,
-            reload: (sysclk + TIMER_HZ / 2) / TIMER_HZ - 1,
-        }
-    }
-}
-
-impl<const TIMER_HZ: u32> Clock for Systick<TIMER_HZ> {
-    type T = u32;
-
-    const SCALING_FACTOR: Fraction = Fraction::new(1, TIMER_HZ);
-
-    #[inline(always)]
-    fn try_now(&self) -> Result<Instant<Self>, Error> {
-        // The instant is always valid
-        Ok(Instant::new(self.cnt))
+        Systick { systick, cnt: 0 }
     }
 }
 
 impl<const TIMER_HZ: u32> Monotonic for Systick<TIMER_HZ> {
     const DISABLE_INTERRUPT_ON_EMPTY_QUEUE: bool = false;
 
+    type Instant = fugit::TimerInstantU64<TIMER_HZ>;
+    type Duration = fugit::TimerDurationU64<TIMER_HZ>;
+
+    fn now(&mut self) -> Self::Instant {
+        if self.systick.has_wrapped() {
+            self.cnt += 1;
+        }
+
+        Self::Instant::from_ticks(self.cnt)
+    }
+
     unsafe fn reset(&mut self) {
-        self.systick.set_clock_source(SystClkSource::Core);
-        self.systick.set_reload(self.reload);
         self.systick.clear_current();
         self.systick.enable_counter();
     }
 
-    fn set_compare(&mut self, _val: &Instant<Self>) {
+    #[inline(always)]
+    fn set_compare(&mut self, _val: Self::Instant) {
         // No need to do something here, we get interrupts every tick anyways.
     }
 
+    #[inline(always)]
     fn clear_compare_flag(&mut self) {
         // NOOP with SysTick interrupt
     }
 
+    #[inline(always)]
+    fn zero() -> Self::Instant {
+        Self::Instant::from_ticks(0)
+    }
+
     fn on_interrupt(&mut self) {
-        // Increase the counter every overflow.
-        self.cnt += 1;
+        if self.systick.has_wrapped() {
+            self.cnt += 1;
+        }
     }
 }
